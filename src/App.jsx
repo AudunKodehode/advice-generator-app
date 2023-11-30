26a58e1ebe4a83c034f75b25d95272487d2af2aa
AudunKodehode <audunkodehode@gmail.com>
2023-11-30 18:54:36 +0100

    *

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.css'
+import dice from '/public/images/icon-dice.svg'
+import { useState } from 'react'
+import paterndiv from '/public/images/pattern-divider-desktop.svg'
+function App() {
+  const [advice, setAdvice] = useState('')
+  const [adviceNumber, setAdviceNumber] = useState('')
+  function getAdvice(){
+   let data = fetch('https://api.adviceslip.com/advice')
+   data.then((res) => res.json())
+   .then((data) => {
+    console.log(data.slip.advice);
+    setAdvice(`“${data.slip.advice}”`);
+    setAdviceNumber(data.slip.id);
+   })
+  }
+  getAdvice();
+  return (
+    <>
+    <div className="app">
+      <div className="adviceNumber">ADVICE #{adviceNumber}</div>
+      <div className="adviceContainer">
+        <div className="adviceText">{advice}</div>
+      </div>
+
+      <div className="pattern-divider-desktop">
+      <img src={paterndiv} alt="" />
+      </div>
+    </div>
+    
+    <div className="diceContainer">
+        <button onClick={() => getAdvice()}><img className='diceImg' src={dice} alt="" /></button>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
